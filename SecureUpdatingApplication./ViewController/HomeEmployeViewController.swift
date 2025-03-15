2e158236d64ba7f8a9e70a647937555daca221fa
Lindokuhle Khumalo <klindokuhle1@gmail.com>
2025-03-15 23:33:19 +0200

    Added success toast message view

--- SecureUpdatingApplication./ViewController/HomeEmployeViewController.swift
+++ SecureUpdatingApplication./ViewController/HomeEmployeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class EmployeViewController: UIViewController {
+class HomeEmployeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
